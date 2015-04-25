bfe5a7b31e82b1937b26c532649dab5e2b0a3576
atrakeur <atrakeur@gmail.com>
2015-04-25 12:25:31 +0200

    Added basic layout for CVUI

---
+++ src/main/java/com/atrakeur/web/restclient/ui/CVUI.java
@@ -0,0 +1,11 @@
+package com.atrakeur.web.restclient.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by Valentin on 25/04/2015.
+ */
+public class CVUI {
+    private JPanel panel;
+    private JTextField textField1;
+}
